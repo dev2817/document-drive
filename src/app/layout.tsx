a2c130920fccfd6701a96eefdd0c791c17eadd14
devsh17 <dev.shah@thegatewaycorp.co.in>
2024-11-22 12:24:42 +0530

    adding analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from 'next/font/google'
 import "./globals.css";
 import React from "react";
+import { Analytics } from '@vercel/analytics/next';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -25,6 +26,7 @@ export default function RootLayout({
         className={`${poppins.variable} font-poppins antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
